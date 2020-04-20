16508cf2fcfc0ca7725e056fb4bbd070a83aa69b
DwikyValentino <dwikyvalentino147@gmail.com>
2020-04-20 15:33:53 +0700

    UTS CRUD Gudang Barang

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,13 +59,31 @@
                                 @endif
 
                                 <div class="col-6">
-                                    <button class="btn btn-primary px-4">Login</button>
+                                    {{-- <button class="btn btn-primary px-4">Login</button> --}}
                                 </div>
                                 <div class="col-6 text-right">
-                                    <button class="btn btn-link px-0" type="button">Forgot password?</button>
+                                    {{-- <button class="btn btn-link px-0" type="button">Forgot password?</button> --}}
                                 </div>
                             </div>
                         </form>
+                        <div>
+                            {{-- @if (Route::has('register'))
+                                <a href="{{ route('auth.register') }}" class="btn btn-primary">Register</a>
+                            @endif --}}
+                            @if (Route::has('login'))
+                            <div class="center links">
+                                @auth
+                                    <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
+                                @else
+                                    <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+            
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" class="btn btn-primary">Register</a>
+                                    @endif
+                                @endauth
+                            </div>
+                            @endif
+                        </div>
                     </div>
                 </div>
                 <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
